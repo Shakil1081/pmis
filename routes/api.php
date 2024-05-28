5673a0155e8e91a1584b7fde34f9c58cba79abad
SHakil Hossain <shakilaust81@gmail.com>
2024-05-28 20:48:18 +0600

    Delete API

--- routes/api.php
+++ routes/api.php
@@ -1,47 +1,47 @@
 <?php
 
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], function () {
-    // Divisions
-    Route::apiResource('divisions', 'DivisionsApiController');
+// Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], function () {
+//     // Divisions
+//     Route::apiResource('divisions', 'DivisionsApiController');
 
-    // District
-    Route::apiResource('districts', 'DistrictApiController');
+//     // District
+//     Route::apiResource('districts', 'DistrictApiController');
 
-    // Designation
-    Route::apiResource('designations', 'DesignationApiController');
+//     // Designation
+//     Route::apiResource('designations', 'DesignationApiController');
 
-    // // Employee List
-    // Route::post('employee-lists/media', 'EmployeeListApiController@storeMedia')->name('employee-lists.storeMedia');
-    Route::apiResource('employee-lists', 'EmployeeListApiController');
+//     // // Employee List
+//     // Route::post('employee-lists/media', 'EmployeeListApiController@storeMedia')->name('employee-lists.storeMedia');
+//     Route::apiResource('employee-lists', 'EmployeeListApiController');
 
-    // Grade
-    Route::apiResource('grades', 'GradeApiController');
+//     // Grade
+//     Route::apiResource('grades', 'GradeApiController');
 
-    // Upazila
-    Route::apiResource('upazilas', 'UpazilaApiController');
+//     // Upazila
+//     Route::apiResource('upazilas', 'UpazilaApiController');
 
-    // Joininginfo
-    Route::apiResource('joininginfos', 'JoininginfoApiController');
+//     // Joininginfo
+//     Route::apiResource('joininginfos', 'JoininginfoApiController');
 
-    // Project Revenuelone
-    Route::apiResource('project-revenuelones', 'ProjectRevenueloneApiController');
+//     // Project Revenuelone
+//     Route::apiResource('project-revenuelones', 'ProjectRevenueloneApiController');
 
-    // Project Revenue Exam
-    // Route::post('project-revenue-exams/media', 'ProjectRevenueExamApiController@storeMedia')->name('project-revenue-exams.storeMedia');
-    Route::apiResource('project-revenue-exams', 'ProjectRevenueExamApiController', ['except' => ['destroy']]);
+//     // Project Revenue Exam
+//     // Route::post('project-revenue-exams/media', 'ProjectRevenueExamApiController@storeMedia')->name('project-revenue-exams.storeMedia');
+//     Route::apiResource('project-revenue-exams', 'ProjectRevenueExamApiController', ['except' => ['destroy']]);
 
-    // Achievementschools Universities
-    Route::apiResource('achievementschools-universities', 'AchievementschoolsUniversitiesApiController');
+//     // Achievementschools Universities
+//     Route::apiResource('achievementschools-universities', 'AchievementschoolsUniversitiesApiController');
 
-    // Project
-    Route::apiResource('projects', 'ProjectApiController');
+//     // Project
+//     Route::apiResource('projects', 'ProjectApiController');
 
-    // Forest States
-    Route::apiResource('forest-states', 'ForestStatesApiController');
+//     // Forest States
+//     Route::apiResource('forest-states', 'ForestStatesApiController');
 
-    // Forest Ranges
-    Route::apiResource('forest-ranges', 'ForestRangesApiController');
+//     // Forest Ranges
+//     Route::apiResource('forest-ranges', 'ForestRangesApiController');
 
-    // Forest Beats
-    Route::apiResource('forest-beats', 'ForestBeatsApiController');
-});
+//     // Forest Beats
+//     Route::apiResource('forest-beats', 'ForestBeatsApiController');
+// });
