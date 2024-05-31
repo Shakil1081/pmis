84b67912d22ea4115aa99a87e300cffe887992ec
SHakil Hossain <shakilaust81@gmail.com>
2024-06-01 01:28:01 +0600

    Update pdf.php

--- config/pdf.php
+++ config/pdf.php
@@ -3,7 +3,7 @@
 return [
     'mode'                     => '',
     'format'                   => 'A4',
-    'default_font_size'        => '12',
+    'default_font_size'        => '14',
     'default_font'             => 'sans-serif',
     'margin_left'              => 10,
     'margin_right'             => 10,
