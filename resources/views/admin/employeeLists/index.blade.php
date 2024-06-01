7d6a95d11a22da4962a659a32f8e0e8e308352d7
SHakil Hossain <shakilaust81@gmail.com>
2024-06-01 15:36:39 +0600

    Update index.blade.php

--- resources/views/admin/employeeLists/index.blade.php
+++ resources/views/admin/employeeLists/index.blade.php
@@ -122,7 +122,6 @@
                                     'trainings',
                                     'travelRecords',
                                     'foreigntravelpersonals',
-                                    'socialassprattachments',
                                     'extracurriculams',
                                     'otherservicejobs',
                                     'languages',
