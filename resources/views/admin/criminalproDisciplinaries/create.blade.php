7617dc12b9f9a698807d42c62c2df7fa12b1d690
SHakil Hossain <Shakil1081@users.noreply.github.com>
2024-06-05 13:15:05 +0600

    dd

--- resources/views/admin/criminalproDisciplinaries/create.blade.php
+++ resources/views/admin/criminalproDisciplinaries/create.blade.php
@@ -89,6 +89,38 @@
                                     class="help-block">{{ trans('cruds.criminalproDisciplinary.fields.remarks_helper') }}</span>
                             </div>
 
+
+                            <div id="action-fields">
+            <div class="action-field">
+                <div class="form-group">
+                    <label class="required" for="govt_order_no_0">{{ trans('cruds.diciplinaryAction.fields.govt_order_no') }}</label>
+                    <input class="form-control {{ $errors->has('govt_order_no.0') ? 'is-invalid' : '' }}" type="text" name="govt_order_no[]" id="govt_order_no_0" value="{{ old('govt_order_no.0', '') }}" required>
+                    @if($errors->has('govt_order_no.0'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('govt_order_no.0') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.diciplinaryAction.fields.govt_order_no_helper') }}</span>
+                </div>
+                <div class="form-group">
+                    <label class="required" for="govt_order_date_0">{{ trans('cruds.diciplinaryAction.fields.govt_order_date') }}</label>
+                    <input class="form-control date {{ $errors->has('govt_order_date.0') ? 'is-invalid' : '' }}" type="text" name="govt_order_date[]" id="govt_order_date_0" value="{{ old('govt_order_date.0') }}" required>
+                    @if($errors->has('govt_order_date.0'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('govt_order_date.0') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.diciplinaryAction.fields.govt_order_date_helper') }}</span>
+                </div>
+                <button type="button" class="btn btn-danger remove-field">Remove</button>
+            </div>
+        </div>
+
+        <div class="form-group">
+        <button type="button" id="add-field" class="btn btn-primary">Add More</button>
+
+        </div>
+
                             <div class="form-group">
                                 <button class="btn btn-danger" type="submit">
                                     {{ trans('global.save') }}
@@ -100,6 +132,43 @@
             </div>
         </div>
     </div>
+
+    <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        let fieldCount = 1;
+
+        document.getElementById('add-field').addEventListener('click', function () {
+            let container = document.getElementById('action-fields');
+            let newField = document.querySelector('.action-field').cloneNode(true);
+
+            newField.querySelectorAll('input').forEach(input => {
+                input.value = '';
+                input.id = input.id.replace(/\d+/, fieldCount);
+            });
+
+            newField.querySelectorAll('label').forEach(label => {
+                label.setAttribute('for', label.getAttribute('for').replace(/\d+/, fieldCount));
+            });
+
+            newField.querySelectorAll('.invalid-feedback').forEach(div => div.remove());
+            newField.querySelectorAll('.is-invalid').forEach(input => input.classList.remove('is-invalid'));
+
+            container.appendChild(newField);
+            fieldCount++;
+        });
+
+        document.getElementById('action-fields').addEventListener('click', function (e) {
+            if (e.target && e.target.matches('button.remove-field')) {
+                if (document.querySelectorAll('.action-field').length > 1) {
+                    e.target.closest('.action-field').remove();
+                } else {
+                    alert('At least one set of fields must be present.');
+                }
+            }
+        });
+    });
+</script>
+
 @endsection
 
 @section('scripts')
