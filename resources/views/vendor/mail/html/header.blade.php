71b5d981ba3e5dd6601b853e50b62258d77c949c
SHakil Hossain <shakilaust81@gmail.com>
2024-06-09 16:41:12 +0600

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,9 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="https://bfis.bforest.gov.bd/srms/assets/images/logo1.png" class="logo" alt="Laravel Logo">
-            @else
-                {{ $slot }}
-            @endif
+
+            <img src="https://bfis.bforest.gov.bd/srms/assets/images/logo1.png" class="logo" alt="Laravel Logo">
+
         </a>
     </td>
 </tr>
