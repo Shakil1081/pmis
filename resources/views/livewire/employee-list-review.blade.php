da93f1a9a9355872fedae162495394721a3669db
SHakil Hossain <shakilaust81@gmail.com>
2024-06-10 14:06:33 +0600

    Update employee-list-review.blade.php

--- resources/views/livewire/employee-list-review.blade.php
+++ resources/views/livewire/employee-list-review.blade.php
@@ -143,10 +143,10 @@
                         </div>
                         <div class="col text-end">
                             <div class="btn-group">
-                                {{-- <button wire:click="approve({{ $empID }})"
+                                <button wire:click="approve({{ $empID }})"
                                     class="approve-button btn btn-sm btn-success">
                                     Approve
-                                </button> --}}
+                                </button>
                                 <a href="{{ route('admin.commonemployeeshow', ['id' => $empID]) }}"
                                     class="btn btn-sm btn-outline-dark">
                                     {{ trans('global.edit') }}
